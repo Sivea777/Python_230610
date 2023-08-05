5e9df150eec3df9ea7ee0494cad9cd522ab629e0
Sivea777 <136140303+Sivea777@users.noreply.github.com>
2023-08-05 04:02:54 +0000

    modity

--- ex230805_lesson13_1.py
+++ ex230805_lesson13_1.py
@@ -1,19 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import streamlit as st
+
 t = np.arange(0.,1.0,0.05)
-print(t)
+st.write(t)
 
 # 脛度
 t1 = 2 * np.pi * t
-print(t1)
+st.write(t1)
 
 # 角度
 y1 = np.sin(2 * np.pi * t)
 y2 = np.cos(2 * np.pi * t)
-print(y1)
-print(y2)
+st.write(y1)
+st.write(y2)
 
 #畫圖
 figure1 = plt.figure(figsize=(8,4))
 axes1 = figure1.add_subplot()
 axes1.plot(y1)
 axes1.plot(y2)
-plt.show()
+
+# 輸出成網頁
+st.write(figure1)
